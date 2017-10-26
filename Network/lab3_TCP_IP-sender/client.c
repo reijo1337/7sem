50db59e283e7fdae41adb6b8a279b9529a02c38d
Grigory Tantsevov <tantsevov@gmail.com>
2017-10-26 20:52:58 +0300

    lab3 Network

---
+++ Network/lab3_TCP_IP-sender/client.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define TRUE 1
+#define FALSE 0
+#define ERROR -1
+#define BUFF_SIZE 128
+
+struct sockaddr_in dest_addr;
+int sock_fd;
+
+void socket_write();
+void socket_read();
+void sigint_handler();
+
+int main(int argc, char* argv[]) {
+    uint32_t option;
+    uint8_t f_ip = FALSE, f_port = FALSE;
+    
+    signal(SIGINT, sigint_handler);
+    
+    while((option=getopt(argc, argv, "i:p:")) != ERROR) {
+        switch(option) {
+            case 'i': 
+                f_ip = inet_pton(AF_INET, optarg, &dest_addr.sin_addr);
+                break;
+            case 'p':
+                dest_addr.sin_port = htons(atoi(optarg));
+                f_port = TRUE;
+                break;
+        }
+    }
+    
+    dest_addr.sin_family = AF_INET;
+    
+    if(!f_ip) {
+        printf("No -i param. Using ip 127.0.0.1\n");
+        inet_pton(AF_INET, "127.0.0.1", &dest_addr.sin_addr);
+    }
+    
+    if(!f_port) {
+        printf("No -p param. Using port 31337\n");
+        dest_addr.sin_port = htons(31337);
+    }
+    
+    if((sock_fd=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == ERROR) {
+        perror("socket()");
+        exit(errno);
+    }
+
+    if(connect(sock_fd, (struct sockaddr*) &dest_addr, sizeof(dest_addr)) == ERROR) {
+        perror("connect()");
+        exit(errno);
+    }
+    
+    switch(fork()) {
+        case ERROR:
+            perror("fork()");
+            exit(errno);
+        case 0:
+            socket_write();
+            break;
+        default:
+            socket_read();
+            break;
+    }
+    
+    close(sock_fd);
+    wait(NULL);
+    return (EXIT_SUCCESS);
+}
+
+void socket_write() {
+    char* out_buffer = malloc(BUFF_SIZE);
+    ssize_t bytes;
+    
+    while(fgets(out_buffer, BUFF_SIZE, stdin) != NULL) {
+        bytes = send(sock_fd, out_buffer, strlen(out_buffer), 0);
+        
+        if(bytes == ERROR) {
+            perror("send()");
+            break;
+        } else if(bytes == 0) {
+            printf("Server connection lost!\n");
+            break;
+        }
+        
+        memset(out_buffer, 0, BUFF_SIZE);
+    }
+}
+
+void socket_read() {
+    char* in_buffer = malloc(BUFF_SIZE);
+    ssize_t bytes;
+    
+    while(TRUE) {
+        bytes = recv(sock_fd, in_buffer, BUFF_SIZE, 0);
+        
+        if(bytes == ERROR) {
+            perror("recv()");
+            break;
+        } else if(bytes == 0) {
+            printf("Server connection lost!\n");
+            break;
+        }
+        
+        printf("%s", in_buffer);
+        memset(in_buffer, 0, BUFF_SIZE);
+    }
+}
+
+void sigint_handler() {
+    close(sock_fd);
+    exit(EXIT_SUCCESS);
+}
---
+++ Network/lab3_TCP_IP-sender/server.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func handleConnection(conn net.Conn) {
+	name := conn.RemoteAddr().String()
+
+	fmt.Printf("%+v connected\n", name)
+	conn.Write([]byte("Hello, " + name + "\n\r"))
+
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "Exit" {
+			conn.Write([]byte("Bye\n\r"))
+			fmt.Println(name, "disconnected")
+			break
+		} else if text != "" {
+			fmt.Println(name, "enters", text)
+			conn.Write([]byte("You enter " + text + "\n\r"))
+
+			reqNum, _ := strconv.Atoi(text)
+			if reqNum <= 0 {
+				conn.Write([]byte("Index out of range"))
+				return
+			}
+
+			file, err := os.Open("ip")
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
+
+			i := 0
+			fileScanner := bufio.NewScanner(file)
+
+			for fileScanner.Scan() {
+				ipAdr := fileScanner.Text()
+				i++
+				if i == reqNum {
+					splitedIP := strings.Split(ipAdr, ".")
+
+					// Вывод в двоичном виде
+					intPart, _ := strconv.Atoi(splitedIP[0])
+					conn.Write([]byte(strconv.FormatInt(int64(intPart), 2)))
+					for i := 1; i < len(splitedIP); i++ {
+						intPart, _ := strconv.Atoi(splitedIP[i])
+						conn.Write([]byte("." + strconv.FormatInt(int64(intPart), 2)))
+					}
+					conn.Write([]byte("\n"))
+
+					// Вывод в восмиричном виде
+					intPart, _ = strconv.Atoi(splitedIP[0])
+					conn.Write([]byte(strconv.FormatInt(int64(intPart), 8)))
+					for i := 1; i < len(splitedIP); i++ {
+						intPart, _ := strconv.Atoi(splitedIP[i])
+						conn.Write([]byte("." + strconv.FormatInt(int64(intPart), 8)))
+					}
+					conn.Write([]byte("\n"))
+
+					// Вывод в шестнадцатиричном виде
+					intPart, _ = strconv.Atoi(splitedIP[0])
+					conn.Write([]byte(strconv.FormatInt(int64(intPart), 16)))
+					for i := 1; i < len(splitedIP); i++ {
+						intPart, _ := strconv.Atoi(splitedIP[i])
+						conn.Write([]byte("." + strconv.FormatInt(int64(intPart), 16)))
+					}
+					conn.Write([]byte("\n"))
+					return
+				}
+			}
+			conn.Write([]byte("Index out of range"))
+		}
+	}
+}
+
+func main() {
+	listner, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listner.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+}
